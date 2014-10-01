054cb8520cfab58d98d887a93ea635aadcb1847e
lvottero <louis.vot@gmail.com>
2014-10-01 12:26:40 -0400

    moving files to python/vtool

---
+++ python/vtool/run_process_manager.py
@@ -0,0 +1,35 @@
+import sys
+
+import qt_ui
+
+import util
+import util_file
+
+import process_manager.ui_process_manager as ui_process_manager
+
+
+
+def main(directory = None):
+    
+    window = None
+    
+    if not util.is_in_maya():
+        window = ui_process_manager.ProcessManagerWindow()   
+        window.show()
+        return window
+        
+    if util.is_in_maya():
+        import maya_lib.ui
+        maya_lib.ui.process_manager()
+        
+    
+
+if __name__ == '__main__':
+    
+    
+    
+    APP = qt_ui.build_qt_application(sys.argv)
+    
+    WINDOW = main()
+    
+    sys.exit(APP.exec_())
---
+++ python/vtool/util_email.py
@@ -0,0 +1,73 @@
+import smtplib
+import email
+import os
+from email.MIMEText import MIMEText
+
+
+class Email(object):
+    
+    def __init__(self, user = None, password = None):
+        self.user = user
+        self.password = password
+
+        self.list_of_emails = []
+        self.server = None
+        
+        self._setup_message()
+
+    def _setup_server(self):
+        self.server = smtplib.SMTP()
+        self.server.set_debuglevel(1)
+    
+    def _setup_message(self):
+        self.message = email.MIMEMultipart.MIMEMultipart()
+        self.message['From'] = self.user
+        self.message['Subject'] = ''
+        self.message['To'] = []
+        
+    def set_subject(self, subject):
+        self.message['Subject'] = subject
+
+    def add_message(self, text):
+        self.message.attach(MIMEText(text))
+
+    def send(self, list_of_emails):
+        joined = email.Utils.COMMASPACE.join(list_of_emails)
+        self.message['To'] = joined
+        self._setup_server()
+        
+        
+        print self.server.sendmail(self.user, list_of_emails, self.message.as_string())
+        
+        self.server.quit()
+
+class Gmail(Email):
+    
+    def _setup_server(self):
+        super(Gmail, self)._setup_server()
+        smtp_host = 'smtp.gmail.com'
+        smtp_port = 587
+        
+        self.server.connect(smtp_host,smtp_port)
+        self.server.ehlo()
+        self.server.starttls()
+        self.server.login(self.user, self.password)
+        
+        
+class BeanfieldEmail(Email):
+    
+    def __init__(self, user):
+        super(BeanfieldEmail, self).__init__(user, '')
+                
+    def _setup_server(self):
+        self.server = smtplib.SMTP('smtp.beanfield.com')
+        self.server.set_debuglevel(1)
+        
+        #super(BeanfieldEmail, self)._setup_server()
+        #smtp_host = 'smtp.beanfield.com'
+        #smtp_port = 25
+        #self.server.connect(smtp_host,smtp_port)
+        #print self.server.ehlo()
+        #self.server.starttls()
+        #print self.server.ehlo()    
+        #self.server.login(self.user, self.password)
