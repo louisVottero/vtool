9dadbcf6cc1835c4ab91c0c6d410172ba7d47bf1
louisVottero <louis.vot@gmail.com>
2014-12-03 21:59:49 -0500

    updating uv eyelid sphere

--- python/vtool/maya_lib/rigs_dev.py
+++ python/vtool/maya_lib/rigs_dev.py
@@ -72,6 +72,113 @@ class EyeLidSphereRig(util.BufferRig):
                 cmds.parent(group_ik, self.setup_group)
                 
                 inc+=1
+
+class EyeLidSphereRig2(util.BufferRig):
+    
+    def __init__(self, description, side):
+        
+        super(EyeLidSphereRig2, self).__init__(description, side)
+        
+        self.radius = 1
+        self.horizontal_sections = 10
+        self.vertical_sections = 10
+                
+    def _create_nurbs_sphere(self):
+        
+        self.surface = cmds.sphere( ch = False, o = True, po = False, ax = [0, 1, 0], radius = self.radius, nsp = 4, n = 'surface_%s' % util.inc_name(self._get_name()) )[0]
+        
+        util.MatchSpace(self.buffer_joints[0], self.surface).translation_rotation()
+        cmds.refresh()
+        cmds.parent(self.surface, self.locator_group)
+        
+        cmds.rotate(-90, 90, 0, self.surface, r = True, os = True)
+        
+        
+    def _add_follicle(self, u_value, v_value, locator = False):
+        
+        follicle = util.create_surface_follicle(self.surface, self._get_name(), u_value, v_value )
+        
+        if locator:
+            locator = cmds.spaceLocator(n = util.inc_name(self._get_name('locatorFollicle')))[0]
+        
+            util.MatchSpace(self.sub_locator_group, locator).translate()
+            cmds.parent(locator, self.sub_locator_group)
+            
+            cmds.makeIdentity(locator, t = True, apply = True)
+        
+        return follicle, locator
+        
+    def _create_locator_group(self):
+        
+        locator_group = cmds.group(em = True, n = util.inc_name(self._get_name('group', 'locator')))
+        sub_locator_group = cmds.group(em = True, n = util.inc_name(self._get_name('group', 'sub_locator')))
+        
+        cmds.parent(sub_locator_group, locator_group)
+        util.MatchSpace(self.buffer_joints[0], locator_group).translation_rotation()
+        
+        cmds.setAttr('%s.scaleX' % locator_group, (self.radius*2) )
+        cmds.setAttr('%s.scaleY' % locator_group, (self.radius*4) )
+        cmds.setAttr('%s.scaleZ' % locator_group, (self.radius*4) )
+        
+        cmds.setAttr('%s.translateX' % sub_locator_group, -0.5)
+        cmds.setAttr('%s.translateY' % sub_locator_group, -0.5)
+        cmds.setAttr('%s.translateZ' % sub_locator_group, 1)
+        
+        self.locator_group = locator_group
+        self.sub_locator_group = sub_locator_group
+        
+    def set_horizontal_sections(self, int_value):
+        self.horizontal_sections = int_value
+        
+    def set_vertical_sections(self, int_value):
+        self.vertical_sections = int_value
+    
+    def create(self):
+        super(EyeLidSphereRig2, self).create()
+        
+        self._create_locator_group()
+        self._create_nurbs_sphere()
+        
+        center_joint = self.joints[0]
+        """
+        v_value = 0.5
+        
+        section_value = 1.0/self.horizontal_sections
+        u_value = 0
+        
+        for inc in range(0, self.horizontal_sections):
+            
+            u_value += section_value
+            
+            sub_section_value = float(v_value)/self.vertical_sections 
+            sub_v_value = v_value
+            multiply_value = 1
+            self.first_folicle = None
+            
+            for inc in range(0, self.vertical_sections):
+                
+                locator_state = False
+                
+                if not self.first_folicle:
+                    locator_state = True
+            
+                folicle, locator = self.add_follicle(u_value, sub_v_value, locator_state)
+                
+                if self.first_folicle:
+                    cmds.connectAttr('%s.parameterV' % self.first_folicle, '%s.parameterV' % folicle, multiply_value)
+                
+                if not self.first_folicle:
+                    self.first_folicle = folicle
+                    cmds.setAttr('%s.translateX' % locator, u_value)
+                    cmds.setAttr('%s.translateY' % locator, sub_v_value)
+                    
+                    cmds.connectAttr('%s.translateX' % locator, '%s.parameterU' % folicle)
+                    cmds.connectAttr('%s.translateY' % locator, '%s.parameterV' % folicle)
+                
+                sub_v_value -= sub_section_value
+                multiply_value -= sub_section_value
+        """     
+                
                 
 def create_joint_slice( center_joint, description, radius = 2, sections = 1, axis = 'X'):
     
