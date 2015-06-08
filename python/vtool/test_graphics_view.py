ffd0babd6a780286ea6493eed41c35960ecb1c61
louisVottero <louis.vot@gmail.com>
2015-06-08 07:07:55 -0400

    renamed

---
+++ python/vtool/test_graphics_view.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2014 Louis Vottero louis.vot@gmail.com    All rights reserved.
+
+import sys
+
+import util
+import qt_ui
+
+def main(directory = None):
+    
+    window = None
+    
+    if not util.is_in_maya():
+        window = qt_ui.BasicGraphicsView()
+           
+        window.show()
+        return window
+    
+    if util.is_in_maya():
+        window = qt_ui.BasicGraphicsView()
+           
+        window.show()
+        return window  
+
+if __name__ == '__main__':
+    
+    APP = qt_ui.build_qt_application(sys.argv)
+    
+    WINDOW = main()
+    
+    sys.exit(APP.exec_())
