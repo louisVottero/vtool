2576c6de457859d679a516d0a2ee3d593872887f
Louis Vottero <louis.vot@gmail.com>
2017-04-12 14:08:28 -0400

    match_geo_blendshape now allows for curves

--- python/vtool/data.py
+++ python/vtool/data.py
@@ -2378,7 +2378,9 @@ def read_ldr_file(filepath):
     
     found = []
     
-    matrix_scale = maya_lib.api.Matrix([0.1, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 1.0])
+    scale = 0.001
+    
+    matrix_scale = maya_lib.api.Matrix([scale, 0.0, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, 0.0, 1.0])
     #matrix_180 = maya_lib.api.Matrix( [0.1, 0.0, 0.0, 0.0, 0.0, -0.1, 1.2246467991473533e-17, 0.0, 0.0, -1.2246467991473533e-17, -0.1, 0.0, 0.0, 0.0, 0.0, 1.0] )
     #matrix_180 = maya_lib.api.Matrix( [1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.2246467991473532e-16, 0.0, 0.0, -1.2246467991473532e-16, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0])
     
@@ -2399,6 +2401,7 @@ def read_ldr_file(filepath):
                        float(matrix_values[0]), float(matrix_values[1]), float(matrix_values[2]), 1]
         
         matrix = maya_lib.api.Matrix(matrix_list)
+        
         
         matrix_scaled = matrix.api_object * matrix_scale.api_object
         
@@ -2433,12 +2436,10 @@ def read_lxfml_file(filepath):
             groups = model.findall('Group')
             
             for group in groups:
-                print group
                 
                 parts = group.findall('Part')
                 
                 for part in parts:
-                    print part
                     
                     position = [0,0,0]
                     angle_vector = [0,0,0]
