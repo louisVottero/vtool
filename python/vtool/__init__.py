678b390acfa5660bc1ce35bda54d17ba63c76fb4
Louis Vottero <louis.vot@gmail.com>
2017-08-09 23:32:13 -0400

    added temp log environment variables

--- python/vtool/__init__.py
+++ python/vtool/__init__.py
@@ -11,4 +11,6 @@ os.environ['VETALA_SETTINGS'] = ''
 os.environ['VETALA_CURRENT_PROCESS'] = ''
 os.environ['VETALA_PRE_SAVE_INITIALIZED'] = 'False'
 os.environ['VETALA_SAVE_COMMENT'] = ''
-
+os.environ['VETALA_TEMP_LOG'] = ''
+os.environ['VETALA_KEEP_TEMP_LOG'] = 'False'
+os.environ['VETALA_LAST_TEMP_LOG'] = ''
