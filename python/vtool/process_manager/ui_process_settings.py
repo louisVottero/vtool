60f55e89e89d942ce79d75bbd73a933fa19f9017
Louis Vottero <louis.vot@gmail.com>
2018-09-22 18:51:47 -0400

    process settings

---
+++ python/vtool/process_manager/ui_process_settings.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2016 Louis Vottero louis.vot@gmail.com    All rights reserved.
+
+from vtool import qt_ui, qt
+from vtool import util
+
+class ProcessSettings(qt_ui.BasicWidget):
+    
+    def __init__(self):
+        
+        self.directory = None
+        
+        super(ProcessSettings, self).__init__()
+        
+    def set_directory(self, directory):
+        self.directory = directory
+        self.name_widget.set_directory(self.directory)
+    
+    def set_active(self, bool_value):
+        
+        self.name_widget.set_active(bool_value)
+    
+    def _build_widgets(self):
+        
+        self.name_widget = qt_ui.DefineControlNameWidget(self.directory)
+        
+        self.main_layout.addWidget(self.name_widget)
