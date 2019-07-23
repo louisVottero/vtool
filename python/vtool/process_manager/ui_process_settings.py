61b2b74b168721c4083a50aa07562a182ba60372
Louis Vottero <louis.vot@gmail.com>
2019-07-22 23:02:35 -0400

    added process backup widget

--- python/vtool/process_manager/ui_process_settings.py
+++ python/vtool/process_manager/ui_process_settings.py
@@ -2,6 +2,7 @@
 
 from vtool import qt_ui, qt
 from vtool import util
+from vtool.process_manager import process
 
 class ProcessSettings(qt_ui.BasicWidget):
     
@@ -16,6 +17,7 @@ class ProcessSettings(qt_ui.BasicWidget):
     def set_directory(self, directory):
         self.directory = directory
         self.name_widget.set_directory(self.directory)
+        self.management_widget.set_directory(self.directory)
     
     def set_active(self, bool_value):
         
@@ -23,6 +25,54 @@ class ProcessSettings(qt_ui.BasicWidget):
     
     def _build_widgets(self):
         
+        self.management_widget = ManagementWidget()
+        
         self.name_widget = qt_ui.DefineControlNameWidget(self.directory)
         
+        self.management_widget.collapse_group()
+        self.name_widget.collapse_group()
+        
+        self.main_layout.addWidget(self.management_widget)
         self.main_layout.addWidget(self.name_widget)
+        
+        
+class ManagementWidget(qt_ui.Group):
+    
+    def __init__(self, name = 'Management'):
+        super(ManagementWidget, self).__init__(name)
+        
+        self.directory = None
+        
+        
+    def _build_widgets(self):
+        
+        backup = qt.QPushButton('Backup This Process')
+        prune_version = qt.QPushButton('Prune Old Versions')
+        
+        backup.clicked.connect(self._backup_process)
+        prune_version.clicked.connect(self._prune_versions)
+        
+        self.main_layout.addWidget(backup)
+        self.main_layout.addWidget(prune_version)
+        
+        
+    def _backup_process(self):
+        
+        if not self.directory:
+            return
+        
+        process_inst = process.Process()
+        process_inst.set_directory(self.directory)
+        process_inst.backup()
+        
+    
+    def _prune_versions(self):
+        
+        pass
+    
+    def set_directory(self, directory):
+        
+        self.directory = directory
+        
+        
+        
