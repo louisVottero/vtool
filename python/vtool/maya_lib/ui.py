5d6b4b01fb79da342609673e8d8f7140ab1dc43a
lvottero <lvottero@work-208.squeeze.lan>
2019-09-06 18:44:04 -0400

    work for normal maya docking

--- python/vtool/maya_lib/ui.py
+++ python/vtool/maya_lib/ui.py
@@ -4,6 +4,8 @@ import maya.cmds as cmds
 import maya.utils
 
 
+from maya.app.general.mayaMixin import MayaQWidgetBaseMixin, MayaQWidgetDockableMixin
+
 from vtool import qt_ui, qt
 from vtool import util, util_file
 
@@ -35,12 +37,26 @@ def load_into_tool_manager(window):
 
 ui_core.new_tool_signal.signal.connect(load_into_tool_manager) 
 
+def add_tool_tab(window):
+    
+    if ToolManager._last_instance:
+        ToolManager._last_instance.add_tab(window, window.title)
+    
+    if not ToolManager._last_instance:
+        ui_core.create_window(window)    
 
+
+def process_manager():
+    
+    window = ui_rig.process_manager()
+    
+    add_tool_tab(window)
     
 def pose_manager(shot_sculpt_only = False):
-    ui_rig.pose_manager()
     
-
+    window = ui_rig.pose_manager(shot_sculpt_only)
+    
+    add_tool_tab(window)
 
 def shape_combo():
     
@@ -68,10 +84,10 @@ def tool_manager(name = None, directory = None):
         
     manager = ToolManager(name)
     manager.set_directory(directory)
+    manager.show()
+    #funct = lambda : ui_core.create_window(manager)
     
-    funct = lambda : ui_core.create_window(manager)
-    
-    maya.utils.executeDeferred(funct)
+    #maya.utils.executeDeferred(funct)
     
     return manager
 
@@ -94,25 +110,34 @@ def process_manager(directory = None):
         window.set_code_directory(directory)
     
     return window
+
+
+#class ToolManager(ui_core.MayaDockMixin, qt_ui.BasicWidget):
+class ToolManager(ui_core.MayaDockMixin,qt.QWidget):
+    title = (util.get_custom('vetala_name', 'VETALA') + ' HUB')
+    _last_instance = None
     
-class ToolManager(ui_core.MayaDirectoryWindow):
-    title = title = (util.get_custom('vetala_name', 'VETALA') + ' HUB')
-    
-    def __init__(self, name = None):
+    def __init__(self,name = None):
+        
+        self.__class__._last_instance = self
+        
         if name:
             self.title = name
-    
+        
         self.default_docks = []
         self.docks = []
         
         super(ToolManager, self).__init__()
         
+        self.setWindowTitle(self.title)
+        
+        self.main_layout = qt.QVBoxLayout()
+        self.setLayout(self.main_layout)
+        self._build_widgets()
+        
+        
     def _build_widgets(self):
 
-        #self.setMinimumWidth(300)
-        
-        self.main_widget.setSizePolicy(qt.QSizePolicy.Minimum, qt.QSizePolicy.Minimum)
-        
         self.main_layout.setAlignment(qt.QtCore.Qt.AlignTop)
         
         header_layout = qt.QHBoxLayout()
@@ -124,76 +149,30 @@ class ToolManager(ui_core.MayaDirectoryWindow):
         
         self.main_layout.addLayout(header_layout)
         
-        self.dock_window = DockWindow()
-        
-        
+        self.dock_window = Dock()
         
         self.main_layout.addWidget(self.dock_window)
         
-        #self.modeling_widget = ui_model.ModelManager()
         self.rigging_widget = ui_rig.RigManager()
-        #self.animation_widget = ui_anim.AnimationManager()
-        ##self.shot_widget = qt.QWidget()
-        #self.fx_widget = ui_fx.FxManager()
         
-        #self.add_tab(self.modeling_widget, 'MODEL')
         self.add_tab(self.rigging_widget, 'RIG')
-        
-        #icon = qt_ui.get_icon('vetala.png')
-        #tab_bar.setTabIcon(0, icon)
-        
-        self.default_docks[0].raise_()
-        
-        #this was one when there was a model dock before rig
-        #self.default_docks[1].raise_()
-        
-    def _get_tab_bar(self):
-        return self.dock_window._get_tab_bar()
-        
-    def _tab_moved(self, from_index, to_index):
-        
-        self.dock_window.tabifyDockWidget(self.docks[from_index], self.docks[to_index])
-        return
-        
-    def _add_default_tabs(self):
-        
-        for dock in self.default_docks:
-            try:
-                dock.close()
-            except:
-                pass
-            
-        self.default_docks = []
-        
-        self.add_tab(self.modeling_widget, 'MODEL')
-        self.add_tab(self.rigging_widget, 'RIG')
-        self.add_tab(self.animation_widget, 'ANIMATE')
-        self.add_tab(self.fx_widget, 'FX')
-        
-        
-    def _close_tab(self, index):
-        
-        self.tab_widget.removeTab(index)
-        
+
     def add_tab(self, widget, name):
         
-        dock = self.add_dock(widget, name)
-        
-        dock.setFeatures(dock.DockWidgetMovable| dock.DockWidgetFloatable)
-        self.default_docks.append(dock)
+        self.add_dock(widget, name)
         
     def add_dock(self, widget , name):
         
-        dock = self.dock_window.add_dock(widget, name)
-        
-        return dock
+        self.dock_window.add_dock(widget, name)
         
     def set_directory(self, directory):
         
-        super(ToolManager, self).set_directory(directory)
+        #super(ToolManager, self).set_directory(directory)
+        
         self.rigging_widget.set_directory(directory)
         
-class DockWindow(qt_ui.BasicWindow):
+        
+class Dock(ui_core.MayaBasicMixin,qt_ui.BasicWindow):
     
     def __init__(self, name = None):
         
@@ -201,11 +180,9 @@ class DockWindow(qt_ui.BasicWindow):
         
         self.connect_tab_change = True
         
-        super(DockWindow, self).__init__()
+        super(Dock, self).__init__()
         
         self.tab_change_hide_show = True
-        
-        
         
     def _get_tab_bar(self):
         
@@ -235,10 +212,13 @@ class DockWindow(qt_ui.BasicWindow):
         self.centralWidget().hide()
         
         self.setTabPosition(qt.QtCore.Qt.TopDockWidgetArea, qt.QTabWidget.West)
-        self.setDockOptions( self.AnimatedDocks | self.AllowTabbedDocks)
+        self.setDockOptions( self.AllowTabbedDocks)
+        #self.setDockOptions( self.AnimatedDocks | self.AllowTabbedDocks)
         
     def _tab_changed(self, index):
         
+        pass
+        """
         if not self.tab_change_hide_show:
             return
         
@@ -246,18 +226,34 @@ class DockWindow(qt_ui.BasicWindow):
         
         docks[index].hide()
         docks[index].show()
-        
+        """
     def add_dock(self, widget , name):
         
+        
         docks = self._get_dock_widgets()
-            
+        
         for dock in docks:
             if dock.windowTitle() == name:
                 dock.deleteLater()
                 dock.close()
         
-        dock_widget = DockWidget(name, self)
+        
+        
+        old_parent = widget.parent()
+        old_parent_name = None
+        if old_parent:
+            old_parent_name = old_parent.objectName()
+            
+        dock_widget = ui_core.MayaDockWidget(self)
+        dock_widget.setWindowTitle(name)
         dock_widget.setWidget(widget)
+        
+        
+        
+        if old_parent_name and old_parent_name.find('Mixin') > -1:
+            old_parent.close()
+            cmds.deleteUI(old_parent_name)
+        
         
         self.addDockWidget(qt.QtCore.Qt.TopDockWidgetArea, dock_widget)
         
@@ -267,23 +263,18 @@ class DockWindow(qt_ui.BasicWindow):
         dock_widget.show()
         dock_widget.raise_()
         
-        tab_bar = self._get_tab_bar()
+        #tab_bar = self._get_tab_bar()
         
+        """
         if tab_bar:
             if self.connect_tab_change:
                 tab_bar.currentChanged.connect(self._tab_changed)
                 self.connect_tab_change = False
-        
+        """
         return dock_widget
 
     
     
-class DockWidget(qt.QDockWidget):
-    
-    def __init__(self, name, parent):
-        super(DockWidget, self).__init__(name, parent)
-        
-        self.setSizePolicy(qt.QSizePolicy.Minimum, qt.QSizePolicy.Minimum)
-        self.setAllowedAreas(qt.QtCore.Qt.TopDockWidgetArea)
+
         
         
