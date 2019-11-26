67af6561c227d588de67b6abd31df8b6bf62da80
lvottero <lvottero@work-208.squeeze.lan>
2019-11-26 09:00:15 -0500

    added camel_to_underscore

--- python/vtool/util.py
+++ python/vtool/util.py
@@ -1841,6 +1841,10 @@ def show_list_to_string(*args):
     except:
         raise(RuntimeError)
 
+def camel_to_underscore(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
 def show(*args):
     try:
         
