0f7af819e231628c2bb221eaa52edf73003d8af2
lvottero <lvottero@work-208.squeeze.lan>
2020-02-17 10:31:03 -0500

    fix when getting parent in create_ghost_follow_chain

--- python/vtool/maya_lib/space.py
+++ python/vtool/maya_lib/space.py
@@ -3430,7 +3430,7 @@ def create_ghost_follow_chain(transforms):
     
     parent = cmds.listRelatives(transforms[0], parent = True)
     if parent:
-        parent = cmds.duplicate(parent[0], po = True, n = 'ghost_%s' % parent[0])
+        parent = cmds.duplicate(parent[0], po = True, n = 'ghost_%s' % parent[0])[0]
         cmds.parent(parent, w = True)
         
         attr.remove_user_defined(parent)
