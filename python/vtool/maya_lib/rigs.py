0e97d4c05919a79c2008f3ccb9ab962b68fc3b61
lvottero <lvottero@work-208.squeeze.lan>
2020-06-10 15:29:15 -0400

    sub added

--- python/vtool/maya_lib/rigs.py
+++ python/vtool/maya_lib/rigs.py
@@ -1102,6 +1102,7 @@ class SparseRig(JointRig):
         self.follow_parent = False
         self.control_compensate = False
         self.run_function = None
+        self._create_sub_controls = False
         
     def _convert_to_joints(self):
         
@@ -1229,7 +1230,10 @@ class SparseRig(JointRig):
         """
         
         self.run_function = function
-        
+    
+    def set_create_sub_controls(self, bool_value):
+        self._create_sub_controls = bool_value
+    
     def create(self):
         
         super(SparseRig, self).create()
@@ -1284,12 +1288,23 @@ class SparseRig(JointRig):
                 const = cmds.scaleConstraint(joint, xform)
                 cmds.delete(const)
             
+            sub = False
+            if self._create_sub_controls:
+                sub = self._create_control(sub = True)
+                
+                cmds.parent(sub, control.control, r = True)
+            
             if self.attach_joints:
-                cmds.parentConstraint(control_name, joint, mo = True)
+                const_control = control_name
+                
+                if sub:
+                    const_control = sub.control
+                cmds.parentConstraint(const_control, joint, mo = True)
 
-            if self.is_scalable:
-                scale_constraint = cmds.scaleConstraint(control.get(), joint)[0]
-                space.scale_constraint_to_local(scale_constraint)
+                if self.is_scalable:
+                    scale_constraint = cmds.scaleConstraint(const_control, joint)[0]
+                    space.scale_constraint_to_local(scale_constraint)
+                    
             if not self.is_scalable:
                 control.hide_scale_attributes()
             
