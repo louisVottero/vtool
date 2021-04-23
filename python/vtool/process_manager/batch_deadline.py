53c634e7290c9576962d0660a0f952e719dd9145
lvottero <lvottero@marz.vfx>
2021-04-23 16:14:25 -0400

    added batch_deadline.py for deadline submission

---
+++ python/vtool/process_manager/batch_deadline.py
@@ -0,0 +1,19 @@
+from vtool import util
+from vtool.process_manager import process
+
+def main():
+    
+    vetala_dir = util.get_env('VETALA_CURRENT_PROCESS')
+    
+    process_inst = process.Process()
+    process_inst.set_directory(vetala_dir)
+    process_inst.run()
+    
+    comment = 'Deadline Batch build'
+    saved = process_inst.save_data('build', comment)
+        
+    if not saved:
+        util.show('Unable to save contents!!')
+    
+if __name__ == '__main__':
+    main()
