26b74ecd2eb5a603edcef87cea88af2ebc370913
Louis Vottero <louis.vot@gmail.com>
2022-04-25 22:47:02 -0400

    removed empty space

--- python/vtool/maya_lib/deform.py
+++ python/vtool/maya_lib/deform.py
@@ -6499,7 +6499,6 @@ def skin_group(joints, group, dropoff_rate = 4.0):
             pass
 
 def skin_nurbs_from_mesh(source_mesh, target_nurbs):
-        
     mesh = source_mesh
     nurbs = target_nurbs
     
