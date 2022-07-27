a70c090276456e8297c6afb016a915c1d92e3766
Louis Vottero <louis.vot@gmail.com>
2022-07-27 11:25:46 -0400

    testing module dir

--- docs/config.py
+++ docs/config.py
@@ -41,7 +41,7 @@ extensions = [
     'sphinxcontrib.apidoc'
 ]
 
-apidoc_module_dir = '../python'
+apidoc_module_dir = '../python/vtool'
 apidoc_output_dir = 'reference/api'
 apidoc_excluded_paths = ['tests']
 apidoc_separate_modules = True
