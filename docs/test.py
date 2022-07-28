12924e460895366c02aeb382fc721c1cf1585223
Louis Vottero <louis.vot@gmail.com>
2022-07-28 11:09:29 -0400

    added more things to test environment

--- docs/test.py
+++ docs/test.py
@@ -1,1 +1,15 @@
-print('here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!..............!!!!!!!!!!!!!')
+
+import sys
+import os
+
+print(os.getcwd())
+print(__file__)
+print( __dir__)
+
+path = __dir__
+path = path[:-4] + 'python'
+
+sys.path.append()
+from vtool import util
+print('is linux?')
+print(util.is_linux())
