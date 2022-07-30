aa8f55ae8ec538ef5462946b1d5efce853221591
Louis Vottero <louis.vot@gmail.com>
2022-07-30 01:51:39 -0400

    further simplified

--- python/vtool/maya_lib/ui.py
+++ python/vtool/maya_lib/ui.py
@@ -7,11 +7,6 @@ from .. import util, util_file
 
 if util.is_in_maya():
     import maya.cmds as cmds
-    import maya.utils
-    import maya.mel as mel
-
-    from maya.app.general.mayaMixin import MayaQWidgetBaseMixin, MayaQWidgetDockableMixin
-    from maya import OpenMayaUI as omui
 
 from .ui_lib import ui_rig
 from . import ui_core
