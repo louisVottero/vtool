eba03d629ad49c54659d137fede485e60e0b40ad
Louis Votter <louis.vot@gmail.com>
2023-10-04 13:05:17 -0400

    working on crossplatform usd module

--- python/vtool/data.py
+++ python/vtool/data.py
@@ -7,7 +7,7 @@ import json
 import traceback
 import threading
 
-from . import util, util_file
+from . import util, util_file, usd
 
 
 if util.in_maya:
@@ -4197,108 +4197,7 @@ class UsdData(CustomData):
 
     def _data_extension(self):
         return 'usd'
-
-    def _import_houdini(self, filepath):
-        filename = util_file.get_basename_no_extension(filepath)
-        
-        filepath = util_file.fix_slashes(filepath)
-        project_path = filepath.split('.data')[0]
-        if project_path.endswith('/'):
-            project_path = project_path[:-1]
-            
-        project = util_file.get_basename(project_path)
-        
-        obj = hou.node('/obj')
-        geo = obj.node(project)
-        if not geo:
-            geo = obj.createNode('geo', project)
-            
-        usd = geo.createNode('kinefx::usdcharacterimport', 'usd_%s' % filename)
-        usd.parm('usdsource').set(1)
-        usd.parm('usdfile').set(filepath)
-        
-
-    def _import_unreal(self, filepath):
-        project_path  = util.get_env('VETALA_PROJECT_PATH')
-        
-        filename = util_file.get_basename_no_extension(filepath)
-        folder_path = util_file.remove_common_path_simple(project_path, filepath)
-        dirname = util_file.get_dirname(folder_path)
-        index = dirname.find('/.data')
-        if index > -1:
-            dirname = dirname[:index]
-        
-        content_path = util_file.join_path('/Game/Vetala', dirname)
-        game_dir = unreal.Paths.project_content_dir()
-        full_content_path = util_file.join_path(game_dir, 'Vetala')
-        full_content_path = util_file.join_path(full_content_path, dirname)
-        util_file.create_dir(full_content_path)
-        
-        options = unreal.UsdStageImportOptions()
-        options.import_actors = True
-        options.import_geometry = True
-        options.import_skeletal_animations = True
-        options.import_level_sequences = True
-        options.import_materials = True
-        
-        task = unreal.AssetImportTask()
-        task.set_editor_property('save', True)
-        task.set_editor_property('filename', filepath)
-        task.set_editor_property('destination_path', content_path)
-        task.set_editor_property('destination_name', filename)
-        task.set_editor_property('automated', True)
-        task.set_editor_property('options', options)
-        task.set_editor_property('replace_existing', True)
-        
-        asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
-        asset_tools.import_asset_tasks([task])
-        
-        asset_paths = unreal.EditorAssetLibrary.list_assets(content_path, recursive = True)
-        
-        util.show(len(asset_paths))
-        
-        asset_path = util_file.join_path(content_path, filename)
-        
-        
-        unreal.EditorAssetLibrary.save_directory(asset_path, recursive = True, only_if_is_dirty = True)
-        
-        found = []
-        found_control_rig = None
-        found_skeletal_mesh = None
-        for asset_path in asset_paths:
-            
-            package_name = asset_path.split('.')
-            package_name = package_name[0]
-            full_path = unreal.Paths.convert_relative_path_to_full(asset_path)
-            full_path = full_path.replace('/Game/', '')
-            full_path = util_file.join_path(game_dir, full_path)
-            #util.show(full_path)
-            util.show(package_name)
-            found.append(package_name)
-            
-            if unreal_lib.util.is_skeletal_mesh(package_name):
-                found_skeletal_mesh = package_name                    
-            
-            if unreal_lib.util.is_control_rig(package_name):
-                found_control_rig = package_name
-                
-        if found_skeletal_mesh:
-            mesh = unreal_lib.util.get_skeletal_mesh_object(found_skeletal_mesh)
-        if found_control_rig:
-            rig = unreal_lib.util.get_skeletal_mesh_object(found_control_rig)
-        if not found_control_rig and found_skeletal_mesh:
-            rig = unreal_lib.util.create_control_rig_from_skeletal_mesh(mesh)
-            found_skeletal_mesh = mesh.get_outer().get_name()
-            found_control_rig = rig.get_outer().get_name()
-            unreal.EditorAssetLibrary.save_asset(found_control_rig, only_if_is_dirty=True)
-        
-        return found
-        
-        
-        
-    def _import_maya(self, filepath):
-        maya_lib.core.import_usd_file(filepath)
-
+    
     def _export_maya(self, filepath, selection):
         maya_lib.core.export_usd_file(filepath, selection)
 
@@ -4311,18 +4210,9 @@ class UsdData(CustomData):
                 return
             import_file = filepath
         
-        if util.in_houdini:
-            util.show('here!!!')
-            self._import_houdini(filepath)
         
-        if util.in_maya:
-            self._import_maya(filepath)
-            
-        if util.in_unreal:
-            result = self._import_unreal(filepath)
-            return result
-            
-        
+        result = usd.import_file(filepath)
+        return result
             
     def export_data(self, comment, selection = []):
         filepath = self.get_file()        
---
+++ python/vtool/usd.py
@@ -0,0 +1,15 @@
+
+from . import util
+
+usd = None
+
+if util.in_houdini:
+    from .houdini_lib import usd
+if util.in_maya:
+    from .maya_lib import usd
+if util.in_unreal:
+    from .unreal_lib import usd
+
+def import_file(filepath):
+    
+    usd.import_file(filepath)
