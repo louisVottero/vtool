d671e359d6521fc080fb9c70e9e379604c839672
coshiO <77947003+caterpillarAttack@users.noreply.github.com>
2023-10-20 14:02:30 -0400

    Reverting change.

--- python/vtool/maya_lib/ui_lib/ui_corrective.py
+++ python/vtool/maya_lib/ui_lib/ui_corrective.py
@@ -635,7 +635,7 @@ class PoseTreeWidget(BaseTreeWidget):
                 if not entered_item.parent():
                     parent_item = self.invisibleRootItem()
                     
-                if self.drag_parent is not parent_item:
+                if not self.drag_parent is parent_item:
                     
                     index = entered_item.indexOfChild(self.dragged_item)
                     child = entered_item.takeChild(index)
