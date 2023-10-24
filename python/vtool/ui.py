cdfa2ae15903a33749442b35192c28bf16897884
caterpillarAttack <77947003+caterpillarAttack@users.noreply.github.com>
2023-10-24 02:16:41 -0400

    ui logic cleanup

--- python/vtool/ui.py
+++ python/vtool/ui.py
@@ -2,18 +2,16 @@ from . import util
 
 if util.in_houdini:
     from .houdini_lib import ui
-if util.in_maya:
+elif util.in_maya:
     from .maya_lib import ui
-if util.in_unreal:
+elif util.in_unreal:
     from .unreal_lib import ui
 
 
 def process_manager():
     if util.in_houdini:
         ui.process_manager()
-        
-    if util.in_maya:
+    elif util.in_maya:
         ui.tool_manager()
-        
-    if util.in_unreal:
+    elif util.in_unreal:
         ui.process_manager()
