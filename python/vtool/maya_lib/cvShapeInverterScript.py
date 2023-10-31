11eb31dca8e2ce8fe9b6179daf9dea414f9e7087
coshiO <77947003+caterpillarAttack@users.noreply.github.com>
2023-10-30 20:48:21 -0400

    cvShapeInverterScript pep8 cleanup

--- python/vtool/maya_lib/cvShapeInverterScript.py
+++ python/vtool/maya_lib/cvShapeInverterScript.py
@@ -12,7 +12,6 @@ from .. import util
 
 
 def invert(base=None, corrective=None, name=None):
-    
     """@brief Inverts a shape through the deformation chain.
 
     @param[in] base Deformed base mesh.
@@ -20,10 +19,10 @@ def invert(base=None, corrective=None, name=None):
     @param[in] name Name of the generated inverted shape.
     @return The name of the inverted shape.
     """
-    
+
     if not cmds.pluginInfo('cvShapeInverterDeformer.py', query=True, loaded=True):
         cmds.loadPlugin('cvShapeInverterDeformer.py')
-            
+
     cmds.undoInfo(openChunk=True)
     if not base or not corrective:
         sel = cmds.ls(sl=True)
@@ -31,31 +30,31 @@ def invert(base=None, corrective=None, name=None):
             cmds.undoInfo(closeChunk=True)
             raise RuntimeError('Select base then corrective')
         base, corrective = sel
-        
+
     # Get points on base mesh
     basePoints = getPoints(base)
     numPoints = basePoints.length()
 
     # Get points on corrective mesh
-    #correctivePoints = getPoints(corrective)
+    # correctivePoints = getPoints(corrective)
 
     # Get the intermediate mesh
-    shapes = cmds.listRelatives(base, children=True, shapes=True, f = True)
-    
+    shapes = cmds.listRelatives(base, children=True, shapes=True, f=True)
+
     if not shapes:
         raise RuntimeError('No intermediate shape found for %s.' % base)
-    
+
     for s in shapes:
         if cmds.getAttr('%s.intermediateObject' % s) and cmds.listConnections('%s.worldMesh' % s,
-                source=False):
+                                                                              source=False):
             origMesh = s
             break
     else:
         cmds.undoInfo(closeChunk=True)
         raise RuntimeError('No intermediate shape found for %s.' % base)
-    
+
     # Get the component offset axes
-    
+
     origPoints = getPoints(origMesh)
     xPoints = OpenMaya.MPointArray(origPoints)
     yPoints = OpenMaya.MPointArray(origPoints)
@@ -72,42 +71,41 @@ def invert(base=None, corrective=None, name=None):
     setPoints(origMesh, zPoints)
     zPoints = getPoints(base)
     setPoints(origMesh, origPoints)
-    
+
     # Create the mesh to get the inversion deformer
     if not name:
         name = '%s_inverted' % corrective
 
     base_shape = getShape(base)
-    
+
     invertedShape = create_shape_from_shape(base_shape)
-    
+
     # Delete the unnessary shapes
-    
-    
-    shapes = cmds.listRelatives(invertedShape, children=True, shapes=True, fullPath = True)
-    
+
+    shapes = cmds.listRelatives(invertedShape, children=True, shapes=True, fullPath=True)
+
     for s in shapes:
         if cmds.getAttr('%s.intermediateObject' % s):
             cmds.delete(s)
-    
+
     setPoints(invertedShape, origPoints)
-    
+
     # Unlock the transformation attrs
     for attr in 'trs':
         for x in 'xyz':
             cmds.setAttr('%s.%s%s' % (invertedShape, attr, x), lock=False)
-            
+
     cmds.setAttr('%s.visibility' % invertedShape, 1)
-    
+
     deformer = cmds.deformer(invertedShape, type='cvShapeInverter')[0]
-    
+
     # Calculate the inversion matrices
-    
+
     oDeformer = getMObject(deformer)
     fnDeformer = OpenMaya.MFnDependencyNode(oDeformer)
-    
+
     plugMatrix = fnDeformer.findPlug('inversionMatrix', False)
-    
+
     fnMatrixData = OpenMaya.MFnMatrixData()
     for i in range(numPoints):
         matrix = OpenMaya.MMatrix()
@@ -126,20 +124,17 @@ def invert(base=None, corrective=None, name=None):
     plugDeformedPoints = fnDeformer.findPlug('deformedPoints', False)
     plugDeformedPoints.setMObject(oPointData)
 
-    
-
     cmds.connectAttr('%s.outMesh' % getShape(corrective), '%s.correctiveMesh' % deformer)
-    
+
     cmds.setAttr('%s.activate' % deformer, True)
 
     cmds.undoInfo(closeChunk=True)
-    
+
     split_name = invertedShape.split(':')
     if len(split_name) > 1:
         invertedShape = cmds.rename(invertedShape, split_name[-1])
-    
+
     return invertedShape
-    
 
 
 def getShape(node):
@@ -149,7 +144,7 @@ def getShape(node):
     @return The associated shape node.
     """
     if cmds.nodeType(node) == 'transform':
-        shapes = cmds.listRelatives(node, shapes=True, f = True)
+        shapes = cmds.listRelatives(node, shapes=True, f=True)
         if not shapes:
             raise RuntimeError('%s has no shape' % node)
         return shapes[0]
@@ -177,13 +172,11 @@ def getDagPath(node):
     @return The dag path of a node.
     """
     selectionList = OpenMaya.MSelectionList()
-    
+
     selectionList.add(node)
     pathNode = OpenMaya.MDagPath()
     selectionList.getDagPath(0, pathNode)
-    
-    
-    
+
     return pathNode
 
 
@@ -239,67 +232,67 @@ def setMatrixCell(matrix, value, row, column):
 
 
 def create_shape_from_shape(shape):
-    
-    parent = cmds.listRelatives(shape, p = True, f = True)
-    
-    transform = cmds.group(em = True)
-    transform = cmds.ls(transform, l = True)[0]
-    
+    parent = cmds.listRelatives(shape, p=True, f=True)
+
+    transform = cmds.group(em=True)
+    transform = cmds.ls(transform, l=True)[0]
+
     from . import api
 
     api.create_mesh_from_mesh(shape, transform)
-    
+
     add_to_isolate_select([transform])
-    
+
     mesh = transform
 
     if parent:
-        
+
         parent = parent[0]
-        
+
         parent_name = parent.split('|')
         if parent_name:
             parent_name = parent_name[-1]
-            
-        mesh = cmds.rename(mesh, '%s_inverted' % parent_name)    
-        
-        translate = cmds.xform(parent, q = True, ws = True, t = True)
-        rotate = cmds.xform(parent, q = True, ws = True, ro = True)
-        
-        cmds.xform(mesh, ws = True, t = translate)
-        cmds.xform(mesh, ws = True, ro = rotate)
-        
+
+        mesh = cmds.rename(mesh, '%s_inverted' % parent_name)
+
+        translate = cmds.xform(parent, q=True, ws=True, t=True)
+        rotate = cmds.xform(parent, q=True, ws=True, ro=True)
+
+        cmds.xform(mesh, ws=True, t=translate)
+        cmds.xform(mesh, ws=True, ro=rotate)
+
     return mesh
-    
-    
+
+
 def add_to_isolate_select(nodes):
     """
         Add the specified nodes into every viewport's isolate select. 
         This will only work on viewports that have isolate select turned on.
         Use when nodes are not being evaluated because isolate select causes them to be invisible.
     """
-    
+
     if is_batch():
         return
-    
+
     nodes = util.convert_to_sequence(nodes)
-    
+
     model_panels = get_model_panels()
-    
+
     for panel in model_panels:
-        if cmds.isolateSelect(panel, q = True, state = True):
-            for node in nodes: 
-                cmds.isolateSelect(panel, addDagObject = node)
-                
-            #cmds.isolateSelect(panel, update = True)
-            
+        if cmds.isolateSelect(panel, q=True, state=True):
+            for node in nodes:
+                cmds.isolateSelect(panel, addDagObject=node)
+
+            # cmds.isolateSelect(panel, update = True)
+
+
 def is_batch():
     """
     Return True if Maya is in batch mode.
     """
-    
-    return cmds.about(batch = True)
+
+    return cmds.about(batch=True)
+
 
 def get_model_panels():
-    
-    return cmds.getPanel(type = 'modelPanel')
+    return cmds.getPanel(type='modelPanel')
