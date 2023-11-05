72989e0f5d1125448a039f8360a1f84cf5543139
caterpillarAttack <77947003+caterpillarAttack@users.noreply.github.com>
2023-11-05 00:13:37 -0400

    usd pep8 cleanup

--- python/vtool/houdini_lib/usd.py
+++ python/vtool/houdini_lib/usd.py
@@ -1,21 +1,22 @@
 from .. import util_file
 
+
 def import_file(filepath):
     filepath = util_file.fix_slashes(filepath)
     filename = util_file.get_basename_no_extension(filepath)
-    
+
     project_path = filepath.split('.data')[0]
-    
+
     if project_path.endswith('/'):
         project_path = project_path[:-1]
-        
+
     project = util_file.get_basename(project_path)
-    
+
     obj = hou.node('/obj')
     geo = obj.node(project)
     if not geo:
         geo = obj.createNode('geo', project)
-        
+
     usd = geo.createNode('kinefx::usdcharacterimport', 'usd_%s' % filename)
     usd.parm('usdsource').set(1)
     usd.parm('usdfile').set(filepath)
