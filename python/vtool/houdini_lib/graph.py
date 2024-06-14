dda05066c225713163394ed6197b40b0d530c643
Louis Votter <louis.vot@gmail.com>
2024-06-14 13:37:50 -0400

    building base apex/ramen setup in houdini

--- python/vtool/houdini_lib/graph.py
+++ python/vtool/houdini_lib/graph.py
@@ -24,6 +24,25 @@ def build_edit_graph(character_node=None):
     sub_graph.setInput(1, character_node, 1)
 
     edit_graph = sub_graph.createNode('apex::editgraph', 'ramen_apex_edit_graph')
+    pack_folder = sub_graph.createNode('packfolder', 'ramen_apex_pack')
+    invoke_graph = sub_graph.createNode('apex::invokegraph', 'ramen_apex_invoke')
+    edit_graph.setPosition(hou.Vector2(-4, -1))
+    pack_folder.setPosition(hou.Vector2(0, -1))
+    invoke_graph.setPosition(hou.Vector2(0, -2))
+
+    pack_folder.setInput(1, sub_graph.indirectInputs()[0])
+    pack_folder.setInput(2, sub_graph.indirectInputs()[1])
+    invoke_graph.setInput(0, edit_graph, 0)
+    invoke_graph.setInput(1, pack_folder, 0)
+
+    button = pack_folder.parm('reloadnames')
+    button.pressButton()
+
+    pack_folder.parm('name1').set('Base')
+    pack_folder.parm('type1').set('shp')
+
+    pack_folder.parm('name2').set('Base')
+    pack_folder.parm('type2').set('skel')
 
     return sub_graph, edit_graph
 
