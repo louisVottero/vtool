7c46b810cb039c7815c4baa63ce46ecdf2f97594
Louis Votter <louis.vot@gmail.com>
2024-10-16 17:50:53 -0400

    trying << and >> to denote input and output

--- python/vtool/process_manager/process.py
+++ python/vtool/process_manager/process.py
@@ -2141,7 +2141,7 @@ class Process(object):
 
         log.info('Get option: name: %s group: %s with value: %s' % (name, group, value))
 
-        util.show('Accessed - Option: %s, Group: %s, value: %s' % (name, group, value))
+        util.show('>> Option: %s, Group: %s, value: %s' % (name, group, value))
 
         return value
 
@@ -3258,7 +3258,7 @@ class Process(object):
         Returns:
             None
         """
-        util.show('!! Created Runtime Variable: %s, value: %s.' % (name, value))
+        util.show('<< Runtime Variable: %s, value: %s.' % (name, value))
         self.runtime_values[name] = value
 
     def get_runtime_value(self, name):
@@ -3275,7 +3275,7 @@ class Process(object):
         if name in self.runtime_values:
             value = self.runtime_values[name]
 
-            util.show('Accessed - Runtime Variable: %s, value: %s' % (name, value))
+            util.show('>> Runtime Variable: %s, value: %s' % (name, value))
 
             return value
 
@@ -3376,12 +3376,12 @@ class Put(dict):
         except:
             util.warning('Put has no attribute: %s' % attr)
             return
-        
+
         if attr == '__dict__':
             return value
 
         if attr not in self.__dict__['_cache_feedback']:
-            util.show('Accessed - put.%s %s' % (attr,value))
+            util.show('>> put.%s %s' % (attr, value))
             self.__dict__['_cache_feedback'][attr] = None
 
         return value
@@ -3391,10 +3391,9 @@ class Put(dict):
         exec('self.%s = value' % key)
         self.__dict__[key] = value
 
-        
     def __setattr__(self, key, value):
-        
-        util.show('Accessed - put.%s=%s' % (key, value))
+
+        util.show('<< put.%s=%s' % (key, value))
         super(Put, self).__setattr__(key, value)
         self.__dict__['_cache_feedback'][key] = None
 
