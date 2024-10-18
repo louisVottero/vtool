fa70c5cf1c8693cdf8aa2da877522b7d6397ab80
Louis Votter <louis.vot@gmail.com>
2024-10-18 11:59:01 -0400

    process.get_option string conversion to list: only if more than one item

--- python/vtool/process_manager/process.py
+++ python/vtool/process_manager/process.py
@@ -675,6 +675,8 @@ class Process(object):
 
         if util.is_str(value):
             new_value = util.convert_str_to_list(value)
+            if len(new_value) == 1:
+                new_value = new_value[0]
 
         if self._option_result_function:
             new_value = self._option_result_function(new_value, option_name)
