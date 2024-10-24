63649642daa70deaac1627aca08b7558aab3fa25
Louis Votter <louis.vot@gmail.com>
2024-10-24 09:41:02 -0400

    auto load blendshapes in blendshape class

--- python/vtool/maya_lib/blendshape.py
+++ python/vtool/maya_lib/blendshape.py
@@ -39,6 +39,10 @@ class BlendShape(object):
         self.prune_compare_mesh = None
         self.prune_distance = -1
 
+        if cmds.objExists(self.blendshape):
+            self._store_meshes()
+            self._store_targets()
+
     def _store_meshes(self, force=False):
         if not self.blendshape:
             return
