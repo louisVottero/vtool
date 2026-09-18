e8219a3673c3804fb358c39098daac3d016731cc
Louis Vottero <louis.vot@gmail.com>
2026-09-18 02:32:41 -0400

    wasn't the solution

--- python/vtool/maya_lib/ui.py
+++ python/vtool/maya_lib/ui.py
@@ -139,7 +139,7 @@ class ToolManager(ui_core.MayaDirectoryWindowMixin):
         self.main_layout.addLayout(header_layout)
 
         self.rigging_widget = ui_rig.RigManager()
-        self.main_layout.addWidget(self.rigging_widget, 1)
+        self.main_layout.addWidget(self.rigging_widget)
 
     def add_tab(self, widget, name):
         self.add_dock(widget, name)
